15803e583166f38b8220f35e3dad552a7788aaf0
Loren-Quirama <lorenmontiel84@gmail.com>
2025-07-15 08:23:23 -0500

    Ajustes hibernate primer usuario

---
+++ prueba-tecnica/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (first_name,last_name,mobile_phone,password,email,address,date_birth,token) VALUES ('loren','quirama','3005971392','clave123','loren@gmail.com','Calle 123','2004-08-01',NULL);
